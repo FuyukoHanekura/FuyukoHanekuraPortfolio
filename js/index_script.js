3929503f032da14fe930463393e0efe2d5b6b045
BEVIS-SURPRISE_Thomas <bevissurprise.thomas@gmail.com>
2023-12-16 16:10:11 +0100

    feat(index): create homepage

---
+++ js/index_script.js
@@ -0,0 +1,16 @@
+var tablinks = document.getElementsByClassName("tab_link");
+var tabcontents = document.getElementsByClassName("tab_contents");
+
+function opentab(pTabName)
+{
+    for(tablink of tablinks)
+    {
+        tablink.classList.remove("active_link");
+    }
+    for(tabcontent of tabcontents)
+    {
+        tabcontent.classList.remove("active_tab");
+    }
+    event.currentTarget.classList.add("active_link");
+    document.getElementById(pTabName).classList.add("active_tab");
+}
